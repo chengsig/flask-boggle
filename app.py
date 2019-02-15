d739bfa11369c1cd92076734ec1b203f6baeb43c
Cynthia <ivyc8192@gmail.com>
2019-02-15 12:03:55 -0800

    create board on screen

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from boggle import Boggle
 
+from flask import Flask, request, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
 boggle_game = Boggle()
+
+@app.route("/")
+def show_board():
+    """ print board to landing page"""
+
+    board = boggle_game.make_board()
+
+    return render_template("landing.html", board = board)
